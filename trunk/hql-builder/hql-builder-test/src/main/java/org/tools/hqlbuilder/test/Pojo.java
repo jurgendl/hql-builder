535e782ac01841526d2e1b7e4aa71d5a870360b0
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2012-06-14 13:06:28 +0000

    initial commit of converted legacy project

---
+++ trunk/hql-builder/hql-builder-test/src/main/java/org/tools/hqlbuilder/test/Pojo.java
@@ -0,0 +1,71 @@
+package org.tools.hqlbuilder.test;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.hibernate.annotations.AccessType;
+
+@Entity
+@AccessType("field")
+@SuppressWarnings("unused")
+public class Pojo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Integer version;
+
+    private String value;
+
+    private Long getId() {
+        return this.id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private Integer getVersion() {
+        return this.version;
+    }
+
+    private void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).appendSuper(super.toString()).append("id", id).append("version", version)
+                .append("value", value).toString();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if (!(other instanceof Pojo)) {
+            return false;
+        }
+        Pojo castOther = (Pojo) other;
+        return new EqualsBuilder().append(id, castOther.id).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(id).toHashCode();
+    }
+}
