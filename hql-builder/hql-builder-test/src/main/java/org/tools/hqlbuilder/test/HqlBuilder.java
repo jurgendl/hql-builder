948a95c4dd4d22b5ba46c3032b0119fce2b5a818
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2012-06-22 11:32:37 +0000

    cleanup

--- hql-builder/hql-builder-test/src/main/java/org/tools/hqlbuilder/test/HqlBuilder.java
+++ hql-builder/hql-builder-test/src/main/java/org/tools/hqlbuilder/test/HqlBuilder.java
@@ -4,7 +4,7 @@ import org.tools.hqlbuilder.client.HqlBuilderFrame;
 import org.tools.hqlbuilder.client.HqlServiceClientImpl;
 import org.tools.hqlbuilder.common.exceptions.ValidationException;
 
-public class StartTestClient {
+public class HqlBuilder {
     public static void main(String[] args) {
         HqlServiceClientImpl hqlServiceClient = (HqlServiceClientImpl) new org.springframework.context.support.ClassPathXmlApplicationContext(
                 "org/tools/hqlbuilder/test/applicationContext.xml").getBean("hqlServiceClient");
