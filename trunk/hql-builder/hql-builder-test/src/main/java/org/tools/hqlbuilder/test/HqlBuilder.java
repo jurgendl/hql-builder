cc0ea0fce333d69a7483de4fb3981b89a1cf5acf
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2012-07-06 09:21:21 +0000

    tests

--- trunk/hql-builder/hql-builder-test/src/main/java/org/tools/hqlbuilder/test/HqlBuilder.java
+++ trunk/hql-builder/hql-builder-test/src/main/java/org/tools/hqlbuilder/test/HqlBuilder.java
@@ -1,5 +1,7 @@
 package org.tools.hqlbuilder.test;
 
+import java.util.List;
+
 import org.tools.hqlbuilder.client.HqlBuilderFrame;
 import org.tools.hqlbuilder.client.HqlServiceClientImpl;
 import org.tools.hqlbuilder.common.exceptions.ValidationException;
@@ -8,12 +10,18 @@ public class HqlBuilder {
     public static void main(String[] args) {
         HqlServiceClientImpl hqlServiceClient = (HqlServiceClientImpl) new org.springframework.context.support.ClassPathXmlApplicationContext(
                 "org/tools/hqlbuilder/test/applicationContext.xml").getBean("hqlServiceClient");
-        if (hqlServiceClient.execute("from Pojo", Integer.MAX_VALUE).getResults().size() == 0) {
+        List<Object> results = hqlServiceClient.execute("from Pojo", Integer.MAX_VALUE).getResults();
+        if (results.size() == 0) {
             try {
-                hqlServiceClient.save(new Pojo());
+                Pojo object = new Pojo();
+                object.getPlainSet().add("element 1");
+                object.getPlainSet().add("element 2");
+                hqlServiceClient.save(object);
             } catch (ValidationException e) {
                 e.printStackTrace();
             }
+        } else {
+            // System.out.println(results);
         }
         HqlBuilderFrame.start(args, hqlServiceClient);
     }
