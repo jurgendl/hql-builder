c4497242d8ee9504cf95c37b06075a1826432413
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2012-07-25 13:20:32 +0000

    common code

--- trunk/hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/HqlServiceClient.java
+++ trunk/hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/HqlServiceClient.java
@@ -3,9 +3,7 @@ package org.tools.hqlbuilder.client;
 import org.tools.hqlbuilder.common.HqlService;
 
 public interface HqlServiceClient extends HqlService {
-
     public abstract void setServiceUrl(String serviceUrl);
 
     public abstract String getServiceUrl();
-
 }
--- trunk/hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/HqlServiceClientImpl.java
+++ trunk/hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/HqlServiceClientImpl.java
@@ -1,17 +1,9 @@
 package org.tools.hqlbuilder.client;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-
-import org.tools.hqlbuilder.common.ExecutionResult;
-import org.tools.hqlbuilder.common.HibernateWebResolver;
+import org.tools.hqlbuilder.common.DelegatingHqlService;
 import org.tools.hqlbuilder.common.HqlService;
-import org.tools.hqlbuilder.common.QueryParameter;
-import org.tools.hqlbuilder.common.exceptions.ValidationException;
 
-public class HqlServiceClientImpl implements HqlServiceClient {
+public class HqlServiceClientImpl extends DelegatingHqlService implements HqlServiceClient {
     private static final long serialVersionUID = 5659734722245348813L;
 
     private HqlService hqlService;
@@ -27,86 +19,6 @@ public class HqlServiceClientImpl implements HqlServiceClient {
     }
 
     @Override
-    public SortedSet<String> getClasses() {
-        return this.hqlService.getClasses();
-    }
-
-    @Override
-    public String getSqlForHql(String hql) {
-        return this.hqlService.getSqlForHql(hql);
-    }
-
-    @Override
-    public ExecutionResult execute(String string, int max, QueryParameter... queryParameters) {
-        return hqlService.execute(string, max, queryParameters);
-    }
-
-    @Override
-    public ExecutionResult execute(String string, int max, List<QueryParameter> queryParameters) {
-        return hqlService.execute(string, max, queryParameters);
-    }
-
-    @Override
-    public HibernateWebResolver getHibernateWebResolver() {
-        return this.hqlService.getHibernateWebResolver();
-    }
-
-    @Override
-    public String getConnectionInfo() {
-        return this.hqlService.getConnectionInfo();
-    }
-
-    @Override
-    public List<String> search(String text, String typeName) {
-        return this.hqlService.search(text, typeName);
-    }
-
-    @Override
-    public List<String> getProperties(String classname) {
-        return this.hqlService.getProperties(classname);
-    }
-
-    @Override
-    public <T> T save(T object) throws ValidationException {
-        return this.hqlService.save(object);
-    }
-
-    @Override
-    public <T> void delete(T object) {
-        this.hqlService.delete(object);
-    }
-
-    @Override
-    public Set<String> getReservedKeywords() {
-        return hqlService.getReservedKeywords();
-    }
-
-    @Override
-    public Map<String, String> getNamedQueries() {
-        return hqlService.getNamedQueries();
-    }
-
-    @Override
-    public List<QueryParameter> findParameters(String hql) {
-        return hqlService.findParameters(hql);
-    }
-
-    @Override
-    public List<String> getPropertyNames(Object key, String[] parts) {
-        return hqlService.getPropertyNames(key, parts);
-    }
-
-    @Override
-    public ExecutionResult execute(String string, QueryParameter... queryParameters) {
-        return hqlService.execute(string, queryParameters);
-    }
-
-    @Override
-    public ExecutionResult execute(String string, List<QueryParameter> queryParameters) {
-        return hqlService.execute(string, queryParameters);
-    }
-
-    @Override
     public String getServiceUrl() {
         return this.serviceUrl;
     }
@@ -117,12 +29,7 @@ public class HqlServiceClientImpl implements HqlServiceClient {
     }
 
     @Override
-    public String getProject() {
-        return this.hqlService.getProject();
-    }
-
-    @Override
-    public void log() {
-        this.hqlService.log();
+    public HqlService getDelegate() {
+        return hqlService;
     }
 }
