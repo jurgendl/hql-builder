bde2c8ffbc0eca4afd5489c57fe6cab9a47ccec7
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2013-06-07 13:12:26 +0000

    release failed again

--- tags/hql-builder-modules-1.0.0/hql-builder-service/src/main/java/org/tools/hqlbuilder/service/HqlWebService.java
+++
@@ -1,7 +0,0 @@
-package org.tools.hqlbuilder.service;
-
-import org.tools.hqlbuilder.common.HqlService;
-
-public interface HqlWebService extends HqlService {
-    //
-}
--- tags/hql-builder-modules-1.0.0/hql-builder-service/src/main/java/org/tools/hqlbuilder/service/ServiceUtils.java
+++
@@ -1,7 +0,0 @@
-package org.tools.hqlbuilder.service;
-
-import org.tools.hqlbuilder.common.CommonUtils;
-
-public class ServiceUtils extends CommonUtils {
-    //
-}
