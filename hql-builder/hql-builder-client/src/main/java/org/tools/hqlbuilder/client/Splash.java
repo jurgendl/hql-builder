e5b417c89e207b513a3200fdefa2dae5e0b8518f
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2013-06-07 14:39:46 +0000

    splashscreen+logo

--- hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/Splash.java
+++ hql-builder/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/Splash.java
@@ -1,8 +1,11 @@
 package org.tools.hqlbuilder.client;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.image.BufferedImage;
 
@@ -16,6 +19,14 @@ public class Splash extends JComponent {
     private float progress;
 
     private final BufferedImage logo;
+
+    private Color color = Color.black;
+
+    private Color colorInv = Color.white;
+
+    private Point textLocation;
+
+    private Rectangle progressBarLocation;
 
     public Splash(BufferedImage logo) {
         this.logo = logo;
@@ -44,11 +55,27 @@ public class Splash extends JComponent {
     @Override
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(color);
         g2.drawImage(this.logo, 0, 0, this);
         if (this.text != null) {
-            g2.drawString(this.text, 10, this.getHeight() - 20 - 5);
+            if (textLocation != null) {
+                g2.drawString(this.text, textLocation.x, textLocation.y);
+            } else {
+                g2.drawString(this.text, 10, this.getHeight() - 20 - 5);
+            }
         }
-        g2.fillRoundRect(10, this.getHeight() - 20, (int) (this.progress * this.getWidth()) - 10 - 10, 10, 5, 5);
+        if (progressBarLocation != null) {
+            g2.setColor(colorInv);
+            g2.fillRoundRect(progressBarLocation.x, progressBarLocation.y, (int) (1.0f * progressBarLocation.width), progressBarLocation.height, 5, 5);
+            g2.setColor(color);
+            g2.fillRoundRect(progressBarLocation.x + 1, progressBarLocation.y + 1, (int) (this.progress * progressBarLocation.width) - 2,
+                    progressBarLocation.height - 2, 5, 5);
+        } else {
+            g2.setColor(colorInv);
+            g2.fillRoundRect(10, this.getHeight() - 20, (int) (1.0f * this.getWidth()) - 10 - 10, 10, 5, 5);
+            g2.setColor(color);
+            g2.fillRoundRect(10 + 1, this.getHeight() - 20 + 1, (int) (this.progress * this.getWidth()) - 10 - 10 - 2, 10 - 2, 5, 5);
+        }
     }
 
     public void setProgress(float f) {
@@ -68,5 +95,30 @@ public class Splash extends JComponent {
         f.setLocationRelativeTo(null);
         f.setVisible(true);
         return f;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        colorInv = new Color(255 - color.getRed(), 255 - color.getGreen(), 255 - color.getBlue());
+    }
+
+    public Point getTextLocation() {
+        return this.textLocation;
+    }
+
+    public void setTextLocation(Point textLocation) {
+        this.textLocation = textLocation;
+    }
+
+    public Rectangle getProgressBarLocation() {
+        return this.progressBarLocation;
+    }
+
+    public void setProgressBarLocation(Rectangle progressBarLocation) {
+        this.progressBarLocation = progressBarLocation;
     }
 }
