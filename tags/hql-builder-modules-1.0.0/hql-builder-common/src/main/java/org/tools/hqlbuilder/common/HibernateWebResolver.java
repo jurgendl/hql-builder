34da31d3342af3ecf5d38c7a390d11c9a5d7194c
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2013-06-07 13:01:39 +0000

    [maven-release-plugin]  copy for tag hql-builder-modules-1.0.0

---
+++ tags/hql-builder-modules-1.0.0/hql-builder-common/src/main/java/org/tools/hqlbuilder/common/HibernateWebResolver.java
@@ -0,0 +1,210 @@
+package org.tools.hqlbuilder.common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HibernateWebResolver {
+    protected Map<String, ClassNode> nodes = new HashMap<String, ClassNode>();
+
+    public List<ClassNode> getClasses() {
+        return new ArrayList<ClassNode>(nodes.values());
+    }
+
+    public List<Property> getProperties(String classname) {
+        return nodes.get(classname).getEdges();
+    }
+
+    public ClassNode getNode(String classname) {
+        return nodes.get(classname);
+    }
+
+    public class ClassNode extends Node<String, String, Property> {
+        protected Class<?> type;
+
+        public ClassNode(String id) {
+            super(id);
+
+            try {
+                type = Class.forName(id);
+            } catch (ClassNotFoundException ex) {
+                throw new IllegalArgumentException(id, ex);
+            }
+        }
+
+        @Override
+        protected Property createEdge(String eid) {
+            return new Property(eid);
+        }
+
+        public Class<?> getType() {
+            return this.type;
+        }
+
+        @Override
+        public String toString() {
+            return type.toString();
+        }
+    }
+
+    public class Property extends Edge<String, String, Property> {
+        protected boolean collection;
+
+        public Property(String id) {
+            super(id);
+        }
+
+        public boolean isCollection() {
+            return this.collection;
+        }
+
+        public void setCollection(boolean collection) {
+            this.collection = collection;
+        }
+    }
+
+    public ClassNode getOrCreateNode(String className) {
+        ClassNode node = nodes.get(className);
+        if (node == null) {
+            node = new ClassNode(className);
+            nodes.put(className, node);
+        }
+        return node;
+    }
+
+    public static class Edge<NodeId, EdgeId, EdgeImpl extends Edge<NodeId, EdgeId, EdgeImpl>> {
+        protected final EdgeId id;
+
+        protected Node<NodeId, EdgeId, EdgeImpl> target;
+
+        public Edge(EdgeId id) {
+            this.id = id;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+            result = prime * result + ((this.target == null) ? 0 : this.target.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            @SuppressWarnings("rawtypes")
+            Edge other = (Edge) obj;
+            if (this.id == null) {
+                if (other.id != null) {
+                    return false;
+                }
+            } else if (!this.id.equals(other.id)) {
+                return false;
+            }
+            if (this.target == null) {
+                if (other.target != null) {
+                    return false;
+                }
+            } else if (!this.target.equals(other.target)) {
+                return false;
+            }
+            return true;
+        }
+
+        public EdgeId getId() {
+            return this.id;
+        }
+
+        @Override
+        public String toString() {
+            return id.toString();
+        }
+
+        public Node<NodeId, EdgeId, EdgeImpl> getTarget() {
+            return this.target;
+        }
+    }
+
+    public static abstract class Node<NodeId, EdgeId, EdgeImpl extends Edge<NodeId, EdgeId, EdgeImpl>> {
+        protected final NodeId id;
+
+        protected final List<EdgeImpl> edges = new ArrayList<EdgeImpl>();
+
+        public Node(NodeId id) {
+            this.id = id;
+        }
+
+        protected abstract EdgeImpl createEdge(EdgeId eid);
+
+        public EdgeImpl addPath(EdgeId eid, Node<NodeId, EdgeId, EdgeImpl> target) {
+            EdgeImpl edge = createEdge(eid);
+            edges.add(edge);
+            edge.target = target;
+            return edge;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            @SuppressWarnings("rawtypes")
+            Node other = (Node) obj;
+            if (this.id == null) {
+                if (other.id != null) {
+                    return false;
+                }
+            } else if (!this.id.equals(other.id)) {
+                return false;
+            }
+            return true;
+        }
+
+        public List<EdgeImpl> getEdges() {
+            return Collections.unmodifiableList(this.edges);
+        }
+
+        public NodeId getId() {
+            return this.id;
+        }
+
+        @Override
+        public String toString() {
+            return id.toString();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nodes.toString();
+    }
+
+    public Map<String, ClassNode> getNodes() {
+        return this.nodes;
+    }
+}
---
+++ tags/hql-builder-modules-1.0.0/hql-builder-common/src/main/java/org/tools/hqlbuilder/common/QueryParameter.java
@@ -0,0 +1,113 @@
+package org.tools.hqlbuilder.common;
+
+import java.io.Serializable;
+
+public class QueryParameter implements Serializable, Comparable<QueryParameter> {
+    private static final long serialVersionUID = 2308140856360992628L;
+
+    private transient Object value;
+
+    private String name;
+
+    private String text;
+
+    private transient String toString;
+
+    public QueryParameter() {
+        super();
+    }
+
+    public QueryParameter(String text, String name, Object value) {
+        this.text = text;
+        this.name = name;
+        this.value = value;
+        afterInit();
+    }
+
+    /**
+     * afterInit
+     */
+    public void afterInit() {
+        StringBuilder sb = new StringBuilder();
+
+        if (name != null) {
+            sb.append(name).append("=");
+        }
+
+        if (value == null) {
+            sb.append("null");
+        } else {
+            String cb = value.getClass().getName().replaceAll("java.lang.", "");
+            sb.append(cb).append(" ").append(value.toString());
+        }
+
+        toString = sb.toString();
+    }
+
+    /**
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return toString;
+    }
+
+    public Object getValue() {
+        return this.value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+        afterInit();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        afterInit();
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+        afterInit();
+    }
+
+    public String getToString() {
+        return this.toString;
+    }
+
+    public void setToString(String toString) {
+        this.toString = toString;
+    }
+
+    /**
+     * 
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(QueryParameter o) {
+        return 0;
+    }
+
+    public void clear() {
+        setValue(null);
+    }
+
+    @Deprecated
+    final public String getType() {
+        return null;
+    }
+
+    @Deprecated
+    final public void setType(@SuppressWarnings("unused") String type) {
+        //
+    }
+}
