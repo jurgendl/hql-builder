cea85c9d8827b2d82ccb9766015ad56aa091567a
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2013-09-24 13:39:23 +0000

    [maven-release-plugin]  copy for tag hql-builder-modules-1.0.3

---
+++ tags/hql-builder-modules-1.0.3/hql-builder-client/src/main/java/org/tools/hqlbuilder/client/SplashHelper.java
@@ -0,0 +1,124 @@
+package org.tools.hqlbuilder.client;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Window;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.imageio.ImageIO;
+
+import org.swingeasy.UIUtils;
+
+public class SplashHelper {
+    private static Splash splash;
+
+    private static Window window;
+
+    private static int step = 0;
+
+    private static String[] stepInfo = {
+            "Loading service",
+            "Checking filesystem",
+            "Setting up Groovy scripting",
+            "Creating GUI",
+            "Loading favorites",
+            "Starting",
+            "" };
+
+    private static StringBuilder splashtimessb;
+
+    private static long splashtimest;
+
+    private static long splashtimesc;
+
+    private static long[] splashtimesd;
+
+    private static long time = 0;
+
+    private static Preferences cfgp;
+
+    private static boolean stopped = false;
+
+    public static void setup() throws IOException {
+        BufferedImage logo = ImageIO.read(SplashHelper.class.getClassLoader().getResourceAsStream("hql-builder-logo.png"));
+        splash = new Splash(logo);
+        splash.setTextLocation(new Point(500, 88));
+        splash.setProgressBarLocation(new Rectangle(294, 82, 200, 4));
+        splash.setColor(Color.white);
+        window = splash.showSplash();
+        window.setAlwaysOnTop(true);
+        splashtimessb = new StringBuilder();
+        splashtimesc = 0;
+        splash.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                _end(1);
+            }
+        });
+    }
+
+    public static void step() {
+        if (step > 0) {
+            splashtimessb.append(System.currentTimeMillis() - time);
+            if (step < splashtimesd.length) {
+                splashtimessb.append(",");
+            }
+        }
+
+        time = System.currentTimeMillis();
+
+        if (!stopped) {
+            splash.setText(HqlResourceBundle.getMessage(stepInfo[step]) + " ...");
+            splash.setProgress((float) splashtimesc / splashtimest);
+        }
+
+        if (step > 0 && step < splashtimesd.length) {
+            splashtimesc += splashtimesd[step];
+        }
+
+        step++;
+    }
+
+    public static void end() {
+        cfgp.put("splashtimes", splashtimessb.toString());
+        _end(100);
+    }
+
+    protected static void _end(int max) {
+        stopped = true;
+        try {
+            float fmax = max;
+            for (int i = 0; i < max; i++) {
+                UIUtils.translucent(window, Math.max(0f, Math.min(1f, (max - i) / fmax)));
+                Thread.sleep(10);
+            }
+        } catch (Exception ex) {
+            //
+        }
+        window.dispose();
+    }
+
+    public static void update(String connectionInfo) {
+        // TODO other connection types
+        String key = connectionInfo.replaceAll("jdbc:oracle:thin", "").replaceAll("\\?", " ").replaceAll(":", " ").replaceAll("@", " ")
+                .replaceAll("/", " ").trim();
+        cfgp = Preferences.userRoot().node(HqlBuilderFrame.PERSISTENT_ID).node(key);
+        splashtimest = 0;
+        String splashtimes = cfgp.get("splashtimes", "1,1,1,1,1,1");
+        String[] sptp = splashtimes.split(",");
+        splashtimesd = new long[sptp.length];
+        int i = 0;
+        for (String p : sptp) {
+            splashtimesd[i] = Long.parseLong(p);
+            splashtimest += splashtimesd[i];
+            i++;
+        }
+
+        splashtimesc += splashtimesd[0];
+    }
+}
