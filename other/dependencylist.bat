0d02558030adb738907b560935d983a1414e5f22
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@41790253-deaa-3c9a-dd15-4c60fbcc7e63>
2013-12-12 16:44:08 +0000

    [maven-release-plugin]  copy for tag hql-builder-parent-2.0.2

--- other/dependencylist.bat
+++
@@ -1,3 +0,0 @@
-cd..
-cd hql-builder-full
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:list > ../other/dependencylist.txt
--- other/download-dependencies-dev.bat
+++
@@ -1,3 +0,0 @@
-mkdir target
-"%JAVA_HOME%/bin/javac" -d target src/main/java/DownloadDependencies.java
-"%JAVA_HOME%/bin/java" -cp target DownloadDependencies
