e800d1008c443e85d567c3a07ae3b37ee639e8ce
jdlandsh <Jurgen.DeLandsheer@UGent.be>
2015-03-27 11:35:58 +0100

    *

--- hql-builder/hql-builder-orm/src/main/java/org/tools/hqlbuilder/common/CollectionMapper.java
+++
@@ -1,6 +0,0 @@
-package org.tools.hqlbuilder.common;
-
-@FunctionalInterface
-public interface CollectionMapper<S, SC> {
-    SC collect(S sourceProxy);
-}
--- hql-builder/hql-builder-orm/src/main/java/org/tools/hqlbuilder/common/Mapper.java
+++
@@ -1,8 +0,0 @@
-package org.tools.hqlbuilder.common;
-
-import java.util.Map;
-
-@FunctionalInterface
-public interface Mapper<S, T> {
-    void apply(Map<Object, Object> context, S s, T t);
-}
