f37b91f9a9726d6b0348e418ea2ff0260bcf959c
jdlandsh <Jurgen.DeLandsheer@UGent.be>
2015-04-22 12:34:39 +0200

    bash update

--- gitsync.sh
+++ gitsync.sh
@@ -1,5 +1,33 @@
+#!/bin/bash
+
+printf "\033c"
+line="============================================================================="
+echo $line
+echo "> git pull -v"
 git pull -v
+echo $line
+echo "> git add -v -A"
 git add -v -A
-git commit -v -am "*"
-git push -v
-read -p "Press [Enter] key to continue..."
+echo $line
+while [ -z "$message" ]; do
+	read -p "Commit message: " message
+done
+#message=${message:-"update"}
+echo "> git commit -v -am '$message'"
+git commit -v -am "$message"
+echo $line
+while true; do
+    read -p "Push (y/n)?" yn
+    case $yn in
+        [Yy]* ) 
+			echo "> git push -v"
+			git push -v;
+			break;;
+        [Nn]* )
+			break;;
+        * )
+			;;
+    esac
+done
+echo $line
+read -p "Press 'any' key to continue..."
