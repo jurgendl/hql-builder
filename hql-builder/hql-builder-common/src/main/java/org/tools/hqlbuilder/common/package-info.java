c18bcf58bea56d87d93410a1921327faa8519f98
jdlandsh <Jurgen.DeLandsheer@UGent.be>
2015-07-15 11:44:17 +0200

    cleanup

--- hql-builder/hql-builder-common/src/main/java/org/tools/hqlbuilder/common/package-info.java
+++ hql-builder/hql-builder-common/src/main/java/org/tools/hqlbuilder/common/package-info.java
@@ -1,7 +1,4 @@
 @javax.xml.bind.annotation.XmlAccessorOrder(javax.xml.bind.annotation.XmlAccessOrder.ALPHABETICAL)
 @javax.xml.bind.annotation.XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
-// @javax.xml.bind.annotation.XmlSchema(namespace = "http://hql-builder.google.com", xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "hql",
-// namespaceURI = "http://hql-builder.google.com") })
-// @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters({})
 package org.tools.hqlbuilder.common;
 
