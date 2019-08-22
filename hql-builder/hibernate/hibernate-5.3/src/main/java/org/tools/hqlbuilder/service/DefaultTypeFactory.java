20641ae8516356f5ef90690a6e2d3397321a82cc
jurgendl <jurgen.de.landsheer@gmail.com>
2019-08-22 21:15:27 +0200

    hibernate 5.3 and 5.4

---
+++ hql-builder/hibernate/hibernate-5.3/src/main/java/org/tools/hqlbuilder/service/DefaultTypeFactory.java
@@ -0,0 +1,19 @@
+package org.tools.hqlbuilder.service;
+
+//FIXME
+@SuppressWarnings("rawtypes")
+public class DefaultTypeFactory {
+//	private static org.hibernate.type.TypeResolver typeResolver = new org.hibernate.type.TypeResolver();
+//
+//    public static Type resolve(String name) {
+//        return typeResolver.basic(name);
+//    }
+//
+//    public static Type resolve(Class clas) {
+//        return typeResolver.basic(clas.getName());
+//    }
+//
+//    public static Type basic(String name) {
+//        return typeResolver.basic(name);
+//    }
+}
