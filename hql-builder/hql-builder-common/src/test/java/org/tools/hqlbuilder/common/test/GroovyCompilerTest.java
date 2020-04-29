9c53b67d9da75e42924801f693252f23efde62b6
jdlandsh <jurgen.delandsheer@ugent.be>
2020-04-29 11:27:20 +0200

    *

--- hql-builder/hql-builder-common/src/test/java/org/tools/hqlbuilder/common/test/GroovyCompilerTest.java
+++ hql-builder/hql-builder-common/src/test/java/org/tools/hqlbuilder/common/test/GroovyCompilerTest.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.joda.time.LocalDate;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.tools.hqlbuilder.common.GroovyCompiler;
 
 public class GroovyCompilerTest {
@@ -17,35 +17,35 @@ public class GroovyCompilerTest {
 
     @Test
     public void testDefoLong() {
-        Assert.assertEquals(Long.class, GroovyCompiler.eval("1").getClass());
+		Assertions.assertEquals(Long.class, GroovyCompiler.eval("1").getClass());
     }
 
     @Test
     public void testDefoCalc() {
-        Assert.assertEquals(2l, GroovyCompiler.eval("max(1,2)"));
+		Assertions.assertEquals(2l, GroovyCompiler.eval("max(1,2)"));
     }
 
     @Test
     public void testDefoLongCalc() {
-        Assert.assertEquals(Long.class, GroovyCompiler.eval("max(1,2)").getClass());
+		Assertions.assertEquals(Long.class, GroovyCompiler.eval("max(1,2)").getClass());
     }
 
     @Test
     public void testDefoJoda() {
-        Assert.assertEquals(org.joda.time.LocalDate.class, GroovyCompiler.eval("new LocalDate()").getClass());
+		Assertions.assertEquals(org.joda.time.LocalDate.class, GroovyCompiler.eval("new LocalDate()").getClass());
     }
 
     @Test
     public void testDefoCalcArray() {
         Object eval = GroovyCompiler.eval("[1,2]");
-        Assert.assertTrue(List.class.isAssignableFrom(eval.getClass()));
-        Assert.assertEquals(2, ((List<?>) eval).size());
-        Assert.assertEquals(Long.class, ((List<?>) eval).get(0).getClass());
+		Assertions.assertTrue(List.class.isAssignableFrom(eval.getClass()));
+		Assertions.assertEquals(2, ((List<?>) eval).size());
+		Assertions.assertEquals(Long.class, ((List<?>) eval).get(0).getClass());
     }
 
     @Test
     public void testForceInt() {
-        Assert.assertEquals(Integer.class, GroovyCompiler.eval("(int) 2").getClass());
+		Assertions.assertEquals(Integer.class, GroovyCompiler.eval("(int) 2").getClass());
     }
 
     @Test
@@ -59,7 +59,7 @@ public class GroovyCompilerTest {
         m.put("param0", 25l);
         m.put("param1", 84296l);
         m.put("param3$0$", Arrays.asList("/uri/part"));
-        Assert.assertEquals(m, GroovyCompiler.eval("[param0:25, param1:84296, param3$0$:['/uri/part']]"));
+		Assertions.assertEquals(m, GroovyCompiler.eval("[param0:25, param1:84296, param3$0$:['/uri/part']]"));
     }
 
     @Test
@@ -67,7 +67,7 @@ public class GroovyCompilerTest {
         Map<String, Object> m = new HashMap<>();
         m.put("param0", "bronId1%");
         m.put("param1", 20l);
-        Assert.assertEquals(m, GroovyCompiler.eval(toString(m)));
+		Assertions.assertEquals(m, GroovyCompiler.eval(toString(m)));
     }
 
     @Test
@@ -75,16 +75,21 @@ public class GroovyCompilerTest {
         Map<String, Object> m = new HashMap<>();
         m.put("param0", "bronId1");
         m.put("param1", 20l);
-        Assert.assertEquals(m, GroovyCompiler.eval(toString(m)));
+		Assertions.assertEquals(m, GroovyCompiler.eval(toString(m)));
     }
 
     @Test
     public void testLocalDate() {
-        Assert.assertEquals(new LocalDate(2000, 1, 1), GroovyCompiler.eval("new LocalDate(2000,1,1)"));
+		Assertions.assertEquals(new LocalDate(2000, 1, 1), GroovyCompiler.eval("new LocalDate(2000,1,1)"));
         Map<String, Object> m = new HashMap<>();
         m.put("param0", new LocalDate(2000, 1, 1));
         m.put("param1", 20); // isn't a long value as expected because of *1*
-        Assert.assertEquals(m, GroovyCompiler.eval("[param0:new LocalDate(2000,1,1), param1:20]"));// *1* LocalDate doesn't accept long values
+		Assertions.assertEquals(m, GroovyCompiler.eval("[param0:new LocalDate(2000,1,1), param1:20]"));// *1*
+																										// LocalDate
+																										// doesn't
+																										// accept
+																										// long
+																										// values
     }
 
     public static enum GCEnum {
@@ -94,6 +99,6 @@ public class GroovyCompilerTest {
     @Test
     public void testEnum() {
         Object o = GroovyCompiler.eval("org.tools.hqlbuilder.common.test.GroovyCompilerTest.GCEnum.test");
-        Assert.assertEquals(GCEnum.test, o);
+		Assertions.assertEquals(GCEnum.test, o);
     }
 }
